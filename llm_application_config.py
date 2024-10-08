8d8db312ca8a2ed5a0eb7db4b15b30f4a8ee8025
raksha-gaur <96387755+raksha-gaur@users.noreply.github.com>
2024-10-08 16:39:36 +0530

    added config file

---
+++ llm_application_config.py
@@ -0,0 +1,24 @@
+GOOGLE_API_KEY = "your gemini API key"
+
+SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_DANGEROUS",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
